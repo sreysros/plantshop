84d6f0ce66f12a3f77217691fe39b936d3b28558
leak sreysros <sreysros_leak@phillipbank.com.kh>
2021-06-03 17:27:08 +0700

    init project

---
+++ app/screen/ProfileScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const ProfileScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>My Profile</Text>
+        </SafeAreaView>
+    );
+};
+
+export default ProfileScreen;
---
+++ app/tool/Image.tsx
@@ -0,0 +1,10 @@
+const images = {
+  home: require('../assets/image/home/tab/home.png'),
+  favourite: require('../assets/image/home/tab/heart.png'),
+scan: require('../assets/image/home/tab/scan.png'),
+bag: require('../assets/image/home/tab/shopping-bag.png'),
+profile: require('../assets/image/home/tab/user.png'),
+
+}
+export default images;
+
